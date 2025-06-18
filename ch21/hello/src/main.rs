514cb258422d34707dc423c88545f2b5e494fa1d
alinkon0207 <alinkon0207@gmail.com>
2025-06-18 05:37:17 +0000

    Simulated slow request

--- ch21/hello/src/main.rs
+++ ch21/hello/src/main.rs
@@ -2,7 +2,10 @@ use std::{
     fs,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+// --snip--
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -13,18 +16,23 @@ fn main() {
         handle_connection(stream);
     }
 }
-// --snip--
 
 fn handle_connection(mut stream: TcpStream) {
     // --snip--
+
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
+
+    // --snip--
 
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
